9d8fed47d572c43b6de7d4abc467d316abcd89b4
gemechu <adamgemech@gmail.com>
2023-12-23 14:36:11 -0300

    attempt limit added

--- guessing-game.js
+++ guessing-game.js
@@ -12,12 +12,12 @@ const randomInRange = (min, max) => {
 //checks if a number is equal to secretNumber or different
 const checkGuess = (number) => {
     //if number is less than secretNumber display to small
-    if (Number(number) < secretNumber) {
+    if ((number) < secretNumber) {
         console.log('Too Low.');
         return false;
     }
     //if number is greater than secretNumber display to big
-    else if (Number(number) > secretNumber) {
+    else if ((number) > secretNumber) {
         console.log('Too High.')
         return false;
     }
@@ -34,15 +34,16 @@ const askRange = () => {
     //user will supply the range in which she/he wants to play
     return userInput.question('Enter maximum number: ', (max) => {
         userInput.question('Enter minimum number: ', (min) => {
+
             //assign the randomlyy generated value to secretNumber
             secretNumber = randomInRange(min, max);
             userInput.question('Enter a guess: ', (answer) => {
-                if (answer < min || answer > max) {
+                if (Number(answer) < min || Number(answer) > max) {
                     console.log('Your number is out of range, please try again.')
                     userInput.close();
                     return
                 }
-                askGuess(answer);
+                askGuess(Number(answer));
             })
         })
     })
@@ -50,17 +51,18 @@ const askRange = () => {
 
 //request for input from a user;
 const askGuess = (guess) => {
-    let newGuess = guess;
     if (checkGuess(guess)) {
-        console.log('trueeeee')
         //if inputed number is equal to the secret number the user wins.
         userInput.close()
-        return 'You won!'
+        return console.log("You won!")
     }
-    //if not request for another number and continue untill the lucky number is found.
-    return userInput.question('Enter a guess:', (newGuess) => {
-        return askGuess(newGuess);
-    })
+    else {
+        //if not request for another number and continue untill the lucky number is found.
+        return userInput.question('Enter a guess:', (newGuess) => {
+            return askGuess(Number(newGuess));
+        })
+    }
+
 }
 
 askRange();
